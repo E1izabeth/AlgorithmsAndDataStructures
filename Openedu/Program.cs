b440e84cf0e8cee5637a2f64a50c24cfe34fbbe2
Елизавета Кузенкова <kuzenkova.el@yandex.ru>
2019-02-19 20:16:57 +0300

    Add Lab1_1

---
+++ Openedu/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Openedu
+{
+    class Program
+    {
+        static T CreateDelegate<T>(MethodInfo methodInfo)
+        {
+            return (T)(object)Delegate.CreateDelegate(typeof(T), methodInfo);
+        }
+
+        static void Main(string[] args)
+        {
+            var arr = typeof(Program).Assembly.GetTypes();
+            Array.Sort(arr, (a, b) => b.FullName.CompareTo(a.FullName));
+
+            MethodInfo doWorkMethod = null;
+            for (int i = 0; i < arr.Length && doWorkMethod == null; i++)
+                if (arr[i] != typeof(Program))
+                    doWorkMethod = arr[i].GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+
+            if (doWorkMethod != null)
+            {
+                var doWorkToCall = CreateDelegate<Action<string[]>>(doWorkMethod);
+                doWorkToCall(args);
+            }
+            else
+            {
+                throw new ApplicationException("Nothing to do");
+            }
+        }
+    }
+}
---
+++ Openedu/Week1/Lab1_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Openedu.Week1
+{
+    public class Lab1_1
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+
+            using (var file = new System.IO.StreamReader("input.txt"))
+            {
+                var input = file.ReadLine().Split(' ');
+                a = int.Parse(input[0]);
+                b = int.Parse(input[1]);
+            }
+
+            using (var file = new System.IO.StreamWriter("output.txt"))
+            {
+                c = a + b;
+                file.WriteLine(c);
+            }
+        }
+    }
+}
