009f847449a23a05f5369289bb640ddf1897a91c
Елизавета Кузенкова <kuzenkova.el@yandex.ru>
2019-02-22 16:16:28 +0300

    Add Lab1_4

---
+++ Openedu/Week1/Lab1_4.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Openedu.Week1
+{
+    public class Lab1_4
+    {
+        public double[] InsertionSort(double[] array, int[] ids)
+        {
+            double[] result = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && result[j - 1] > array[i])
+                {
+                    var t = ids[j];
+                    ids[j] = ids[j - 1];
+                    ids[j - 1] = t;
+                    result[j] = result[j - 1];
+                    j--;
+                }
+                result[j] = array[i];
+            }
+            return result;
+        }
+
+        private void DoWork(string[] args)
+        {
+            double[] array;
+            int[] ids;
+
+            using (var file = new System.IO.StreamReader("input.txt"))
+            {
+                var count = int.Parse(file.ReadLine());
+                var input = file.ReadLine().Split(' ');
+                array = new double[count];
+                ids = new int[count];
+
+                for (int i = 0; i < ids.Length; i++)
+                {
+                    ids[i] = i + 1;
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    array[i] = double.Parse(input[i]);
+                }
+            }
+
+            var result = this.InsertionSort(array, ids);
+
+            using (var file = new System.IO.StreamWriter("output.txt"))
+            {
+                file.Write($"{ids[0]} {ids[ids.Length / 2]} {ids[ids.Length - 1]}");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+            var app = new Lab1_4();
+            app.DoWork(args);
+        }
+    }
+}
