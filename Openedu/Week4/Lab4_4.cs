443002fea692a74fe20cd0155328f493189c6d59
Елизавета Кузенкова <kuzenkova.el@yandex.ru>
2019-03-12 10:50:20 +0300

    Add Lab4_4

---
+++ Openedu/Week4/Lab4_4.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Openedu.Week4
+{
+    class Lab4_4
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Lab4_4();
+            app.DoWork(args);
+        }
+
+        private void DoWork(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] stdin = File.ReadAllLines("input.txt");
+
+                int[] parameters = stdin[0].Split(' ').Select(t => int.Parse(t)).ToArray();
+                var queue = new Queue<int>();
+
+                var mins = new LinkedList<int>();
+
+                for (var i = 0; i < stdin.Length; i++)
+                {
+                    var temp = stdin[i].Split(' ');
+                    switch (temp[0])
+                    {
+                        case "+":
+                            {
+                                var r = int.Parse(temp[1]);
+                                queue.Enqueue(r);
+
+                                while (mins.Count > 0 && mins.First.Value > r)
+                                {
+                                    mins.RemoveFirst();
+                                }
+
+                                mins.AddFirst(r);
+                                break;
+                            }
+                        case "-":
+                            {
+                                var b = queue.Dequeue();
+
+                                if (mins.Last.Value == b)
+                                {
+                                    mins.RemoveLast();
+                                }
+
+                                break;
+                            }
+                        case "?":
+                            {
+
+                                sw.WriteLine(mins.Last.Value);
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
