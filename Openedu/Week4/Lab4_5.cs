65e438a3be3ce183cddd315c1a7be1e661e94e54
Елизавета Кузенкова <kuzenkova.el@yandex.ru>
2019-03-12 15:10:18 +0300

    Small refactoring Lab4_5

--- Openedu/Week4/Lab4_5.cs
+++ Openedu/Week4/Lab4_5.cs
@@ -29,15 +29,15 @@ namespace Openedu.Week4
 
     public class QuackMachine
     {
-        private ushort[] _registers = new ushort[26];
-        private int _cursor = 0;
-        private Dictionary<string, int> _labels = new Dictionary<string, int>();
-        private string[] _program;
-        private Queue<ushort> _queue = new Queue<ushort>();
+        private static ushort[] _registers = new ushort[26];
+        private static int _cursor = 0;
+        private static Dictionary<string, int> _labels = new Dictionary<string, int>();
+        private static string[] _program;
+        private static Queue<ushort> _queue = new Queue<ushort>();
 
         public QuackMachine(string[] input)
         {
-            this._program = input;
+            _program = input;
             this.RegisterLabels();
         }
 
@@ -47,27 +47,13 @@ namespace Openedu.Week4
             {
                 switch (_program[_cursor][0])
                 {
-                    case '+':
-                        this.Sum();
-                        break;
-                    case '-':
-                        this.Substract();
-                        break;
-                    case '*':
-                        this.Multiply();
-                        break;
-                    case '/':
-                        this.Divide();
-                        break;
-                    case '%':
-                        this.Mod();
-                        break;
-                    case '>':
-                        this.GetRegister(_program[_cursor][1]);
-                        break;
-                    case '<':
-                        this.SetRegister(_program[_cursor][1]);
-                        break;
+                    case '+': this.Sum(); break;
+                    case '-': this.Substract(); break;
+                    case '*': this.Multiply(); break;
+                    case '/': this.Divide(); break;
+                    case '%': this.Mod(); break;
+                    case '>': this.GetRegister(_program[_cursor][1]); break;
+                    case '<': this.SetRegister(_program[_cursor][1]); break;
                     case 'P':
                         if (_program[_cursor].Length == 1)
                             this.PrintValueFromStack();
@@ -80,26 +66,13 @@ namespace Openedu.Week4
                         else
                             this.PrintCharFromRegister(_program[_cursor][1]);
                         break;
-                    case ':':
-                        break;
-                    case 'J':
-                        this.GoTo(_cursor);
-                        break;
-                    case 'Z':
-                        this.GoToIfZeroEqual(_cursor);
-                        break;
-                    case 'E':
-                        this.GoToIfEquals(_cursor);
-                        break;
-                    case 'G':
-                        this.GoToIfMoreThan(_cursor);
-                        break;
-                    case 'Q':
-                        this.Exit();
-                        break;
-                    default:
-                        _queue.Enqueue(ushort.Parse(_program[_cursor]));
-                        break;
+                    case ':': break;
+                    case 'J': this.GoTo(_cursor); break;
+                    case 'Z': this.GoToIfZeroEqual(_cursor); break;
+                    case 'E': this.GoToIfEquals(_cursor); break;
+                    case 'G': this.GoToIfMoreThan(_cursor); break;
+                    case 'Q': this.Exit(); break;
+                    default: _queue.Enqueue(ushort.Parse(_program[_cursor])); break;
                 }
             }
         }
