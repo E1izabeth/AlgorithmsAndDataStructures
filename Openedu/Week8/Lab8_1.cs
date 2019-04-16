4d3a497691a7594f87f44f9e2dbae6449fb321a9
Елизавета Кузенкова <kuzenkova.el@yandex.ru>
2019-04-16 09:03:11 +0300

    Add Lab8_1 and Lab8_2

---
+++ Openedu/Week8/Lab8_1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Openedu.Week8
+{
+    class Lab8_1
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Lab8_1();
+            app.DoWork(args);
+        }
+
+        private void DoWork(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] stdin = File.ReadAllLines("input.txt");
+                var ts = new SortedSet<long>();
+
+                for (int i = 1; i < stdin.Length; i++)
+                {
+                    var key = long.Parse(stdin[i].Split(' ')[1]);
+                    switch (stdin[i][0])
+                    {
+                        case 'A': ts.Add(key); break;
+                        case 'D': ts.Remove(key); break;
+                        case '?':
+                            if (ts.Contains(key))
+                                sw.WriteLine("Y");
+                            else
+                                sw.WriteLine("N");
+                            break;
+                        default: break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Openedu/Week8/Lab8_2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Openedu.Week8
+{
+    class Lab8_2
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Lab8_2();
+            app.DoWork(args);
+        }
+
+        private void DoWork(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] stdin = File.ReadAllLines("input.txt");
+                var list = new LinkedList<string>();
+                var kv = new Dictionary<string, LinkedListNode<string>>();
+
+                for (int i = 1; i < stdin.Length; i++)
+                {
+                    var arr = stdin[i].Split(' ');
+                    var key = arr[1];
+                    switch (arr[0])
+                    {
+                        case "put":
+                            {
+                                var value = arr[2];
+                                if (kv.TryGetValue(key, out var node))
+                                {
+                                    node.Value = value;
+                                }
+                                else
+                                {
+                                    kv.Add(key, list.AddLast(value));
+                                }
+                            }
+                            break;
+                        case "get":
+                            {
+                                if (kv.TryGetValue(key, out var node))
+                                    sw.WriteLine(node.Value);
+                                else 
+                                    sw.WriteLine("<none>");
+                            } break;
+                        case "prev":
+                            {
+                                if (kv.TryGetValue(key, out var node) && node.Previous != null)
+                                    sw.WriteLine(node.Previous.Value);
+                                else
+                                    sw.WriteLine("<none>");
+                            }
+                            break;
+                        case "next":
+                            {
+                                if (kv.TryGetValue(key, out var node) && node.Next != null)
+                                    sw.WriteLine(node.Next.Value);
+                                else
+                                    sw.WriteLine("<none>");
+                            }
+                            break;
+                        case "delete":
+                            {
+                                if (kv.TryGetValue(key, out var node))
+                                {
+                                    kv.Remove(key);
+                                    list.Remove(node);
+                                }
+                            } break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+        }
+    }
+}
