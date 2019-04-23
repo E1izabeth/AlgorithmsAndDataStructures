c8699edcec2652ade493b791f854271c713ca351
Елизавета Кузенкова <kuzenkova.el@yandex.ru>
2019-04-23 12:09:31 +0300

    Add Week 8

---
+++ Openedu/Week8/Lab8_3.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Openedu.Week8
+{
+    public class HashTable
+    {
+        private long _tableSize;
+        private long[] _table;
+
+        public HashTable(long size)
+        {
+            _tableSize = size;
+            _table = new long[size];
+            for (int i = 0; i < size; i++)
+                _table[i] = -1;
+        }
+
+        public bool TryInsert(long key)
+        {
+            long hash = this.GetHash(key);
+            long hash2 = this.GetHash2(key);
+            while (_table[hash] != -1 && _table[hash] != key)
+            {
+                hash = (hash + hash2) % _tableSize;
+                hash2++;
+            }
+            if (_table[hash] == key)
+                return false;
+            _table[hash] = key;
+            return true;
+        }
+
+        private long GetHash(long key)
+        {
+            return Math.Abs(unchecked((int)((long)(key * 47))) ^ (int)((key * 31) >> 32)) % _tableSize;
+        }
+        private long GetHash2(long key)
+        {
+            return Math.Abs(unchecked((int)((long)(key * 113))) ^ (int)((key * 97) >> 32)) % (_tableSize - 1) + 1;
+        }
+    }
+
+    class Lab8_3
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Lab8_3();
+            app.DoWork(args);
+        }
+
+        private void DoWork(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] stdin = File.ReadAllLines("input.txt");
+                var arr1 = stdin[0].Split(' ').Select(s => long.Parse(s)).ToArray();
+                var arr2 = stdin[1].Split(' ').Select(s => long.Parse(s)).ToArray();
+
+                var n = arr1[0];
+                var x = arr1[1];
+                var a = arr1[2];
+                var b = arr1[3];
+
+                var Ac = arr2[0];
+                var Bc = arr2[1];
+                var Ad = arr2[2];
+                var Bd = arr2[3];
+
+                HashTable hashTable = new HashTable(n * 2);
+                for (int i = 0; i < n; i++)
+                {
+                    if (hashTable.TryInsert(x))
+                    {
+                        a = (a + Ad) % 1000;
+                        b = (b + Bd) % 1000000000000000;
+                    }
+                    else
+                    {
+                        a = (a + Ac) % 1000;
+                        b = (b + Bc) % 1000000000000000;
+                    }
+                    x = (x * a + b) % 1000000000000000;
+                }
+
+                sw.WriteLine($"{x} {a} {b}");
+            }
+        }
+    }
+}
---
+++ Openedu/Week8/Lab8_4.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Openedu.Week8
+{
+    class Lab8_4
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Lab8_4();
+            app.DoWork(args);
+        }
+
+        char[] Next(char[] s)
+        {
+            char[] t = (char[])s.Clone();
+            int i = s.Length - 1;
+
+            while (i >= 0 && s[i] == 'z')
+            {
+                s[i] = 'a';
+                --i;
+            }
+
+            if (i >= 0)
+                t[i]++;
+
+            return t;
+        }
+
+        int Hash(char[] s, int x)
+        {
+            int h = 0, i, r = 1, l = s.Length;
+            for (i = 0; i < l; ++i)
+            {
+                h = unchecked(h + unchecked((s[i] - 'a') * r));
+                r = unchecked(r * x);
+            }
+            return h;
+        }
+
+        private void DoWork(string[] args)
+        {
+            int x, L, v, i;
+            char[] s = new char[2500], t;
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                string[] stdin = File.ReadAllLines("input.txt");
+                var N = int.Parse(stdin[0]);
+
+                x = 700;
+                L = 2500;
+                v = 10;
+
+                for (i = 0; i < L; ++i)
+                    s[i] = 'a';
+
+                var ss = new SortedSet<string>();
+                t = s;
+                for (int j = 0; j < N;)
+                {
+                    string xx;
+                    if (this.Hash(t, x) == v && ss.Add(xx = new string(t)))
+                    {
+                        sw.WriteLine(xx);
+                        j++;
+                    }
+                    t = this.Next(t);
+                }
+            }
+        }
+    }
+}
